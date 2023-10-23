138948d7339bb83685fd7b3dba428f2caad8e838
yuzeyang <yuzeyang@xiaoniangao.com>
2023-10-24 02:08:00 +0800

    feat: update package.json

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: yuzeyang yuzeyang@xiaoniangao.com
+ * @Date: 2023-10-24 00:04:38
+ * @LastEditors: yuzeyang yuzeyang@xiaoniangao.com
+ * @LastEditTime: 2023-10-24 01:50:41
+ * @FilePath: /react-music/src/utils/axios.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import axios from 'axios'
 
 const request = axios.create({
